a900d4d8f269ce917aade3cc4f043d00e47be146
hjjae2 <hjjae2@gmail.com>
2021-08-01 01:26:41 +0900

    #2 Element-UI

---
+++ frontend-el/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <common-header />
+    <common-navigator />
+    <nuxt />
+    <common-footer />
+  </v-app>
+</template>
+
+<script>
+import CommonHeader from '~/layouts/header.vue'
+import CommonNavigator from '~/layouts/navigator.vue'
+import CommonFooter from '~/layouts/footer.vue'
+export default {
+  components: {
+    CommonHeader,
+    CommonNavigator,
+    CommonFooter,
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ frontend-el/layouts/navigator.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="12">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: true,
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    },
+  },
+}
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
