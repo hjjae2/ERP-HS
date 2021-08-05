fecde82fee116a56cc2b014d8c02f4fdd5389610
hjjae2 <hjjae2@gmail.com>
2021-08-06 00:55:48 +0900

    #2 ~ing

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-container
+    direction="vertical"
+    style="width: 100%; height: 100%; padding: 0% 5%"
+  >
+    <common-header />
+    <br />
+    <nuxt />
+    <common-footer />
+  </el-container>
+</template>
+
+<script>
+import CommonHeader from '~/layouts/header.vue'
+import CommonFooter from '~/layouts/footer.vue'
+export default {
+  components: {
+    CommonHeader,
+    CommonFooter,
+  },
+}
+</script>
+
+<style scoped>
+.text-primary {
+  color: #409eff;
+}
+</style>
---
+++ frontend/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-card>
+      <div slot="header" class="clearfix">
+        <h2>대시보드</h2>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
