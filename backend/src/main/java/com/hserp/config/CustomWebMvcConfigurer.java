7382bd52477ea0d25273bcde5e9d98d74983ae0d
hjjae2 <hjjae2@gmail.com>
2021-08-14 01:38:12 +0900

    Add configuration about cors

--- backend/src/main/java/com/hserp/config/CustomWebMvcConfigurer.java
+++ backend/src/main/java/com/hserp/config/CustomWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package com.hserp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,13 @@ public class CustomWebMvcConfigurer implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/templates/", "classpath:/static/")
                 .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "http://hs-erp")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
